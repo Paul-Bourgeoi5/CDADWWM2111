ea518ca4832bbf5315af8e46062432c6f3527424
PBourgeois <pbourgeois@arfp.asso.fr>
2021-12-01 12:46:16 +0100

    new version of 1_1_3

--- CSharp/src/Exercices_Algo_To_CSharp/Algo/Exercice1_1_3.cs
+++ CSharp/src/Exercices_Algo_To_CSharp/Algo/Exercice1_1_3.cs
@@ -1,84 +1,107 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Generic;
 
 namespace Algo
 {
     class Exercice1_1_3
     {
-
-        
         /// <summary>
-        /// Split a string into an array of int
+        /// Split a string _sentence depending on specified char into a static array of integers
         /// </summary>
-        /// <param name="_sentence">The string to be split</param>
-        /// <param name="_charWhichSplit">The character which splits the string</param>
-        /// <returns>An int[] containing every integer from a string</returns>
-        private static int[] split(string _sentence, char _charWhichSplit)
+        /// <param name="_sentence"></param>
+        /// <param name="_charWhoSplit"></param>
+        /// <returns>Returns _sentence split depending on _charWhoSplit</returns>
+        private static int[] Split(string _sentence, char _charWhoSplit)
         {
             int howManyInt;
             int numbersCounter;
-            int[] arrayInt;
-            // StringBuilder : une chaine de caractères dont on peut modifier le contenu
+            int charCounter;
+            int[] listInt;
             StringBuilder numberInString;
 
-            howManyInt = 1;
-            for (int charCounter =0;  charCounter <= _sentence.Length - 1; charCounter++)
+            howManyInt = 0;
+            for(charCounter = 0 ; charCounter <= _sentence.Length -1 ; charCounter++)
             {
-                if (_sentence[charCounter].Equals(_charWhichSplit))
+                if (_sentence[charCounter] == _charWhoSplit)
                 {
-                    howManyInt = howManyInt + 1;
+                    //howManyInt = howManyInt + 1;
+                    howManyInt++;
                 }
             }
-
+            howManyInt++;
             numbersCounter = 0;
-            // avec string : numberInString = "";
             numberInString = new StringBuilder();
-            arrayInt = new int[howManyInt];
-            for (int charCounter = 0; charCounter < _sentence.Length; charCounter++)
+            // déclare la taille du tableau listInt et l'instancie
+            listInt = new int[howManyInt];
+
+            for (charCounter = 0; charCounter <= _sentence.Length - 1; charCounter++)
             {
-                if (!_sentence[charCounter].Equals(_charWhichSplit))
+                if (_sentence[charCounter] != _charWhoSplit)
                 {
-                    // avec string : numberInString = numberInString + _sentence[charCounter];
+                    // pourrait être : numberInString += _sentence[charCounter];
                     numberInString.Append(_sentence[charCounter]);
                 }
-                if (_sentence[charCounter].Equals(_charWhichSplit) || 
-                        charCounter == (_sentence.Length -1))
+                if (_sentence[charCounter] == _charWhoSplit || charCounter == (_sentence.Length - 1))
                 {
-                    // avec string : arrayInt[numbersCounter] = int.Parse(numberInString);
-                    arrayInt[numbersCounter] = int.Parse(numberInString.ToString());
-                    numbersCounter = numbersCounter + 1;
-                    // avec string : numberInString = "";
+                    listInt[numbersCounter] = int.Parse(numberInString.ToString());
+                    numbersCounter++;
                     numberInString.Clear();
                 }
             }
+            return listInt;
+        }  
 
-            return arrayInt;
-        }
-
-
-        // static void Main(string[] args)   
-        //remplacé par public static void Run() dans mon système de fichiers
         public static void Run()
         {
             string userInputNumbers;
             int howManyNumbers;
             int sumNumbers;
             int countNumbers;
-            int[] arrayNumbers;
+            int[] listNumbers;
+            //string[] listString;
             double average;
 
-            Console.WriteLine("enter some numbers spaced with ' '");
+            Console.WriteLine("Enter numbers");
             userInputNumbers = Console.ReadLine();
-            arrayNumbers = split(userInputNumbers, ' ');
-            howManyNumbers = arrayNumbers.Length;
+            listNumbers = Split(userInputNumbers, ' ');
+
+            /* Sans utiliser notre propre split :
+             * listString = userInputNumbers.Split(' ');
+            listNumbers = new int[listString.Length];
+            for(int i = 0; i < listString.Length; i++)
+            {
+                listNumbers[i] = int.Parse(listString[i]);
+            }*/
+
+            howManyNumbers = listNumbers.Length;
             sumNumbers = 0;
 
-            for (countNumbers = 0; countNumbers < howManyNumbers ; countNumbers++)
+            for (countNumbers = 0; countNumbers <= howManyNumbers - 1; countNumbers++)
             {
-                sumNumbers = sumNumbers + arrayNumbers[countNumbers];
+                sumNumbers += listNumbers[countNumbers];
             }
-            average = sumNumbers / howManyNumbers;
-            Console.WriteLine($"Average of \"{userInputNumbers}\" is {average}");
+            average = (double)sumNumbers / (double)howManyNumbers;
+            Console.WriteLine($"\nAverage of \"{userInputNumbers}\" is {average}");
         }
+        
+
+        public static void JuniorRun()
+        {
+            string[] numbers;
+            string saisie;
+            double average = 0d;
+
+            Console.WriteLine("Saisissez des nombres séparés par un espace");
+            saisie = Console.ReadLine();
+            numbers = saisie.Split(' ');
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                average += double.Parse(numbers[i]);
+            }
+
+            Console.WriteLine($"La moyenne de vos nombres est {average/(double)numbers.Length}");
+        }
+        
     }
 }
