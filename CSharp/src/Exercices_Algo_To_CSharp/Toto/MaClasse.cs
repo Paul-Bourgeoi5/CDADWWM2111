17d1334b715939c45e8acd6833d7438880c459db
PBourgeois <pbourgeois@arfp.asso.fr>
2021-12-06 16:53:45 +0100

    Exemple d'utilisation de namespace et fonctions

---
+++ CSharp/src/Exercices_Algo_To_CSharp/Toto/MaClasse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toto
+{
+    public class MaClasse
+    {
+        /// <summary>
+        /// Print the message in the console and get user input
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns>User input</returns>
+        public static string GetUserInput(string message)
+        {
+            Console.Write(message+ " ");
+            return Console.ReadLine();
+        }
+    }
+}
